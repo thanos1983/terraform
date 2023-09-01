b4c6c5ac5b558a23b09952d9fe1a1dc353d20370
thanos1983 <7160603thanos1983@users.noreply.github.com>
2023-09-01 11:44:28 +0200

    Updating modules to include for each loops.

--- Azure/modules/LinuxVirtualMachine/outputs.tf
+++ Azure/modules/LinuxVirtualMachine/outputs.tf
@@ -1,51 +1,51 @@
 output "administrator_username" {
   sensitive   = true
   description = "The Administrator Username Windows Virtual Machine."
-  value       = azurerm_linux_virtual_machine.linux_virtual_machine[*].admin_username
+  value       = azurerm_linux_virtual_machine.linux_virtual_machine.admin_username
 }
 
 output "administrator_password" {
   sensitive   = true
   description = "The Administrator Password Windows Virtual Machine."
-  value       = azurerm_linux_virtual_machine.linux_virtual_machine[*].admin_password
+  value       = azurerm_linux_virtual_machine.linux_virtual_machine.admin_password
 }
 
 output "id" {
   description = "The ID of the Windows Virtual Machine."
-  value       = azurerm_linux_virtual_machine.linux_virtual_machine[*].id
+  value       = azurerm_linux_virtual_machine.linux_virtual_machine.id
 }
 
 output "name" {
   description = "The Name assigned to this Virtual Machine."
-  value       = azurerm_linux_virtual_machine.linux_virtual_machine[*].name
+  value       = azurerm_linux_virtual_machine.linux_virtual_machine.name
 }
 
 output "identity" {
   description = "An identity block."
-  value       = azurerm_linux_virtual_machine.linux_virtual_machine[*].identity
+  value       = azurerm_linux_virtual_machine.linux_virtual_machine.identity
 }
 
 output "private_ip_address" {
   description = "The Primary Private IP Address assigned to this Virtual Machine."
-  value       = azurerm_linux_virtual_machine.linux_virtual_machine[*].private_ip_address
+  value       = azurerm_linux_virtual_machine.linux_virtual_machine.private_ip_address
 }
 
 output "private_ip_addresses" {
   description = "A list of Private IP Addresses assigned to this Virtual Machine."
-  value       = azurerm_linux_virtual_machine.linux_virtual_machine[*].private_ip_addresses
+  value       = azurerm_linux_virtual_machine.linux_virtual_machine.private_ip_addresses
 }
 
 output "public_ip_address" {
   description = "The Primary Public IP Address assigned to this Virtual Machine."
-  value       = azurerm_linux_virtual_machine.linux_virtual_machine[*].public_ip_address
+  value       = azurerm_linux_virtual_machine.linux_virtual_machine.public_ip_address
 }
 
 output "public_ip_addresses" {
   description = "A list of the Public IP Addresses assigned to this Virtual Machine."
-  value       = azurerm_linux_virtual_machine.linux_virtual_machine[*].public_ip_addresses
+  value       = azurerm_linux_virtual_machine.linux_virtual_machine.public_ip_addresses
 }
 
 output "virtual_machine_id" {
   description = "A 128-bit identifier which uniquely identifies this Virtual Machine."
-  value       = azurerm_linux_virtual_machine.linux_virtual_machine[*].virtual_machine_id
+  value       = azurerm_linux_virtual_machine.linux_virtual_machine.virtual_machine_id
 }
