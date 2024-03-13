666496059c24309d3e4e708057f2dec811c488cb
atga <atga@nnit.com>
2024-03-13 16:24:08 +0100

    Updating null_resource module.

--- Azure/modules/NullResource/README.md
+++ Azure/modules/NullResource/README.md
@@ -30,8 +30,12 @@ module "sample_null_resource_<project>" {
 - id
 ```
 
+### file Provisioner
+
+The [file Provisioner](https://developer.hashicorp.com/terraform/language/resources/provisioners/file) copies files or directories from the machine running Terraform to the newly created resource. The file provisioner supports both ssh and winrm type connections.
+
 ### remote-exec Provisioner
 
-A very interesting feature that might be useful also [remote-exec Provisioner](https://developer.hashicorp.com/terraform/language/resources/provisioners/remote-exec).
+The [remote-exec Provisioner](https://developer.hashicorp.com/terraform/language/resources/provisioners/remote-exec) invokes a script on a remote resource after it is created. This can be used to run a configuration management tool, bootstrap into a cluster, etc. To invoke a local process, see the local-exec provisioner instead. The remote-exec provisioner requires a connection and supports both ssh and winrm.
 
 ###### More information can be found on the official document [null_resource](https://registry.terraform.io/providers/hashicorp/null/latest/docs/resources/resource) and [terraform/local-exec](https://developer.hashicorp.com/terraform/language/resources/provisioners/local-exec).
--- Azure/modules/NullResource/variables.tf
+++ Azure/modules/NullResource/variables.tf
@@ -4,6 +4,126 @@ variable "triggers" {
   default     = null
 }
 
+variable "type" {
+  description = "The connection type. Valid values are 'ssh' and 'winrm'."
+  type        = string
+  default     = null
+}
+
+variable "user" {
+  description = "The user to use for the connection."
+  type        = string
+  default     = null
+}
+
+variable "password" {
+  description = "The password to use for the connection."
+  type        = string
+  default     = null
+}
+
+variable "host" {
+  description = "The address of the resource to connect to."
+  type        = string
+  default     = null
+}
+
+variable "timeout" {
+  description = "The timeout to wait for the connection to become available. Should be provided as a string (e.g., '30s' or '5m'.)"
+  type        = string
+  default     = null
+}
+
+variable "script_path" {
+  description = "The path used to copy scripts meant for remote execution."
+  type        = string
+  default     = null
+}
+
+variable "private_key" {
+  description = "The contents of an SSH key to use for the connection."
+  type        = string
+  default     = null
+}
+
+variable "certificate" {
+  description = "The contents of a signed CA Certificate. The certificate argument must be used in conjunction with a private_key."
+  type        = string
+  default     = null
+}
+
+variable "agent" {
+  description = "Set to false to disable using ssh-agent to authenticate."
+  type        = bool
+  default     = null
+}
+
+variable "agent_identity" {
+  description = "The preferred identity from the ssh agent for authentication."
+  type        = string
+  default     = null
+}
+
+variable "host_key" {
+  description = "The public key from the remote host or the signing CA, used to verify the connection."
+  type        = string
+  default     = null
+}
+
+variable "target_platform" {
+  description = "The target platform to connect to. Valid values are 'windows' and 'unix'."
+  type        = string
+  default     = null
+}
+
+variable "https" {
+  description = "Set to true to connect using HTTPS instead of HTTP."
+  type        = bool
+  default     = null
+}
+
+variable "insecure" {
+  description = "Set to true to skip validating the HTTPS certificate chain."
+  type        = bool
+  default     = null
+}
+
+variable "use_ntlm" {
+  description = "Set to true to use NTLM authentication rather than default (basic authentication), removing the requirement for basic authentication to be enabled within the target guest."
+  type        = bool
+  default     = null
+}
+
+variable "cacert" {
+  description = "Set to true to use NTLM authentication rather than default (basic authentication), removing the requirement for basic authentication to be enabled within the target guest."
+  type        = string
+  default     = null
+}
+
+variable "port" {
+  description = "The port to connect to."
+  type        = string
+  default     = null
+}
+
+variable "file_source" {
+  description = "The source file or directory. Specify it either relative to the current working directory or as an absolute path."
+  type        = string
+  default     = null
+}
+
+variable "file_content" {
+  description = "The direct content to copy on the destination. If destination is a file, the content will be written on that file."
+  type        = string
+  default     = null
+}
+
+variable "file_destination" {
+  description = "The destination path to write to on the remote system."
+  type        = string
+  default     = null
+}
+
 variable "command" {
   description = "Command to execute"
   type        = string
