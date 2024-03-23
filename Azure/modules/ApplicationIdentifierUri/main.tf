e871c4cf4c470cb5415906842ec44a429a9b2577
atga <atga@nnit.com>
2024-03-23 06:00:22 +0100

    Updating modules.

--- Azure/modules/ApplicationIdentifierUri/main.tf
+++ Azure/modules/ApplicationIdentifierUri/main.tf
@@ -1,4 +1,7 @@
 resource "azuread_application_identifier_uri" "identifier_uri" {
   application_id = var.application_id
   identifier_uri = var.identifier_uri
+  lifecycle {
+    prevent_destroy = true
+  }
 }
