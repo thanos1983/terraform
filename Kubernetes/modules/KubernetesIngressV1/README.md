73631803735ae32b2831805526ebf5cbedc8955e
Athanasios Garyfalos <7160603+thanos1983@users.noreply.github.com>
2024-05-14 18:25:16 +0200

    Adding Ingress kubernetes module.

---
+++ Kubernetes/modules/KubernetesIngressV1/README.md
@@ -0,0 +1,37 @@
+# Ingress is a collection of rules that allow inbound connections to reach the endpoints defined by a backend.
+
+## Sample of use:
+
+```bash
+module "kubectl_manifest_<project>" {
+  source   = "git::https://example.com/kubernetes_ingress_v1_<my_repo>.git"
+  metadata_block = {
+    .
+    .
+    .
+  }
+  .
+  .
+  .
+}
+```
+
+### Variables:
+
+```bash
+- metadata_block (Required)
+- spec_block (Required)
+- wait_for_load_balancer (Opional)
+```
+
+### Output:
+
+```bash
+- status
+- load_balancer
+- ingress
+- load_balancer_ip
+- load_balancer_hostname
+```
+
+###### More information can be found on the official document [kubernetes_ingress_v1](https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/ingress_v1)
---
+++ Kubernetes/modules/KubernetesIngressV1/outputs.tf
@@ -0,0 +1,24 @@
+output "status" {
+  description = "Status is the current state of the Ingress."
+  value       = kubernetes_ingress_v1.ingress_v1.status
+}
+
+output "load_balancer" {
+  description = "LoadBalancer contains the current status of the load-balancer, if one is present."
+  value       = kubernetes_ingress_v1.ingress_v1.load_balancer
+}
+
+output "ingress" {
+  description = "Ingress is a list containing ingress points for the load-balancer."
+  value       = kubernetes_ingress_v1.ingress_v1.ingress
+}
+
+output "load_balancer_ip" {
+  description = "IP is set for load-balancer ingress points that are IP based (typically GCE or OpenStack load-balancers)."
+  value       = kubernetes_ingress_v1.example.status.0.load_balancer.0.ingress.0.ip
+}
+
+output "load_balancer_hostname" {
+  description = "Hostname is set for load-balancer ingress points that are DNS based (typically AWS load-balancers)."
+  value       = kubernetes_ingress_v1.example.status.0.load_balancer.0.ingress.0.hostname
+}
