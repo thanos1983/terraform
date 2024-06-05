50c8a02318255fb8bbbabf0643ba565409f62557
Athanasios Garyfalos <7160603+thanos1983@users.noreply.github.com>
2024-06-05 13:06:40 +0200

    Updating NetworkSecurityGroup module adding security_rule block.

--- Azure/modules/NetworkSecurityGroup/variables.tf
+++ Azure/modules/NetworkSecurityGroup/variables.tf
@@ -13,6 +13,29 @@ variable "location" {
   type        = string
 }
 
+variable "security_rule_blocks" {
+  description = "List of security_rule objects representing security rules, as defined below."
+  type        = list(object({
+    name                                       = string
+    description                                = optional(string)
+    protocol                                   = string
+    source_port_range                          = optional(string)
+    source_port_ranges                         = optional(list(string))
+    destination_port_range                     = optional(string)
+    destination_port_ranges                    = optional(list(string))
+    source_address_prefix                      = optional(string)
+    source_address_prefixes                    = optional(list(string))
+    source_application_security_group_ids      = optional(list(string))
+    destination_address_prefix                 = optional(string)
+    destination_address_prefixes               = optional(list(string))
+    destination_application_security_group_ids = optional(list(string))
+    access                                     = string
+    priority                                   = number
+    direction                                  = string
+  }))
+  default = []
+}
+
 variable "tags" {
   description = "A mapping of tags to assign to the resource."
   type        = map(any)
@@ -22,8 +45,8 @@ variable "timeouts_block" {
   description = "The timeouts block allows you to specify timeouts for certain actions"
   type        = object({
     create = optional(number, 30)
-    read   = optional(number, 5)
     update = optional(number, 30)
+    read   = optional(number, 5)
     delete = optional(number, 30)
   })
   default = null
