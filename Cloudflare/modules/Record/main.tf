9d8fadd91fd85b70c74be4feba0e36290bd741a2
Athanasios Garyfalos <7160603+thanos1983@users.noreply.github.com>
2024-06-17 17:38:09 +0200

    Updating Cloudflare Record module.

--- Cloudflare/modules/Record/main.tf
+++ Cloudflare/modules/Record/main.tf
@@ -58,11 +58,7 @@ resource "cloudflare_record" "record" {
 
   provisioner "local-exec" {
     when    = destroy
-    command = "bash ${path.module}/scripts/cleanup.sh $ZONEID $BEARER_TOKEN"
-    environment = {
-      ZONEID       = var.zone_id
-      BEARER_TOKEN = var.bearer_token
-    }
+    command = "bash ${path.module}/scripts/cleanup.sh ${self.zone_id}"
   }
 
   dynamic "timeouts" {
--- Cloudflare/modules/Record/scripts/cleanup.sh
+++ Cloudflare/modules/Record/scripts/cleanup.sh
@@ -1,4 +1,6 @@
-curl --silent "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records?per_page=50000" --header "Authorization: Bearer $BEARER_TOKEN" | jq --raw-output '.result[].id' | while read id
+zoneid=$1
+bearer=$TF_VAR_CLOUDFLARE_API_KEY
+curl --silent "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?per_page=50000" --header "Authorization: Bearer $bearer" | jq --raw-output '.result[].id' | while read id
 do
-  curl --silent --request DELETE "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records/$id" --header "Authorization: Bearer $BEARER_TOKEN"
+  curl --silent --request DELETE "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$id" --header "Authorization: Bearer $bearer"
 done
