27d0d7c0e939107d1d1bb2f25ceca164081ee757
Athanasios Garyfalos <7160603+thanos1983@users.noreply.github.com>
2024-06-27 13:29:58 +0200

    Updating KubernetesPersistentVolumeClaimV1 output file.

--- Kubernetes/modules/KubernetesPersistentVolumeClaimV1/outputs.tf
+++ Kubernetes/modules/KubernetesPersistentVolumeClaimV1/outputs.tf
@@ -1,4 +1,9 @@
 output "id" {
-  description = "The ID of this resource."
+  description = "The id of the kubernetes persistent volume claim."
   value       = kubernetes_persistent_volume_claim_v1.persistent_volume_claim.id
+}
+
+output "name" {
+  description = "The name of the kubernetes persistent volume claim."
+  value       = kubernetes_persistent_volume_claim_v1.persistent_volume_claim.metadata.0.name
 }
