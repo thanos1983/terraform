75a505b2ded44415948ab77e8b50959e5165da6e
Athanasios Garyfalos <7160603+thanos1983@users.noreply.github.com>
2024-07-11 18:42:18 +0200

    Removing module KubernetesKnativeDevEventing.

--- Kubernetes/modules/KubernetesKnativeDevEventing/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "id" {
-  description = "The id of the resource."
-  value       = data.k8s_operator_knative_dev_knative_eventing_v1beta1_manifest.knative_eventing.id
-}
--- Kubernetes/modules/KubernetesKnativeDevEventing/versions.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_providers {
-    k8s = {
-      source = "metio/k8s"
-    }
-  }
-}
