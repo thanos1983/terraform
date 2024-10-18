3c77af746db88078f70d3dbb55db649312e43209
Athanasios Garyfalos <7160603+thanos1983@users.noreply.github.com>
2024-10-18 10:34:11 +0200

    Updating SubnetRouteTableAssociation.

--- Azure/modules/SubnetRouteTableAssociation/main.tf
+++ Azure/modules/SubnetRouteTableAssociation/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_subnet_route_table_association" "subnet_route_table_association" {
-  route_table_id = ""
-  subnet_id      = ""
+  route_table_id = var.route_table_id
+  subnet_id      = var.subnet_id
 
   dynamic "timeouts" {
     for_each = var.timeouts_block[*]
