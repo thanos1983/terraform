311e2b126843926c3e01df273732a80f58e76fc4
thanos1983 <7160603+thanos1983@users.noreply.github.com>
2024-11-11 16:21:45 +0100

    Updating Virtual Network Subnet module with extra variables.

--- Azure/modules/VirtualNetworkSubNet/main.tf
+++ Azure/modules/VirtualNetworkSubNet/main.tf
@@ -18,8 +18,11 @@ resource "azurerm_subnet" "subnet" {
     }
   }
 
-  service_endpoints           = var.service_endpoints
-  service_endpoint_policy_ids = var.service_endpoint_policy_ids
+  default_outbound_access_enabled               = var.default_outbound_access_enabled
+  private_endpoint_network_policies             = var.private_endpoint_network_policies
+  private_link_service_network_policies_enabled = var.private_link_service_network_policies_enabled
+  service_endpoints                             = var.service_endpoints
+  service_endpoint_policy_ids                   = var.service_endpoint_policy_ids
 
   dynamic "timeouts" {
     for_each = var.timeouts_block[*]
--- Azure/modules/VirtualNetworkSubNet/variables.tf
+++ Azure/modules/VirtualNetworkSubNet/variables.tf
@@ -30,6 +30,38 @@ variable "delegation_blocks" {
   default = []
 }
 
+variable "default_outbound_access_enabled" {
+  description = "Enable default outbound access to the internet for the subnet."
+  type        = bool
+  validation {
+    condition = contains(["true", "false"], lower(tostring(var.default_outbound_access_enabled)))
+    error_message = "Possible values can only be \"true\" or \"false\"."
+  }
+  default = true
+}
+
+variable "private_endpoint_network_policies" {
+  description = "Enable or Disable network policies for the private endpoint on the subnet."
+  type        = string
+  validation {
+    condition = contains([
+      "Disabled", "Enabled", "NetworkSecurityGroupEnabled", "RouteTableEnabled"
+    ], title(var.private_endpoint_network_policies))
+    error_message = "Possible values can only be \"Disabled\", \"Enabled\", \"NetworkSecurityGroupEnabled\" or \"RouteTableEnabled\"."
+  }
+  default = "Disabled"
+}
+
+variable "private_link_service_network_policies_enabled" {
+  description = "Enable or Disable network policies for the private link service on the subnet."
+  type        = bool
+  validation {
+    condition = contains(["true", "false"], lower(tostring(var.private_link_service_network_policies_enabled)))
+    error_message = "Possible values can only be \"true\" or \"false\"."
+  }
+  default = true
+}
+
 variable "service_endpoints" {
   description = "The list of Service endpoints to associate with the subnet."
   type = list(string)
