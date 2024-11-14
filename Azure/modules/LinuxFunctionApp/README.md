e3fb373d2435969b398c336dd1c747c04229b434
thanos1983 <7160603+thanos1983@users.noreply.github.com>
2024-11-14 16:00:00 +0100

    Addin linux function app module.

---
+++ Azure/modules/LinuxFunctionApp/README.md
@@ -0,0 +1,71 @@
+# Azure Linux Function App
+
+## Sample of use:
+
+```bash
+module "linux_function_app_<project>" {
+  source = "git::https://example.com/linux_function_app_<my_repo>.git"
+  name   = "my_linux_function_app"
+  .
+  .
+  .
+}
+```
+
+### Variables:
+
+```bash
+- location (Required)
+- name (Required)
+- resources_group_name (Required)
+- service_plan_id (Required)
+- site_config_block (Required)
+- app_settings (Optional)
+- auth_settings_block (Optional)
+- auth_settings_v2_block (Optional)
+- backup_block (Optional)
+- builtin_logging_enabled (Optional)
+- client_certificate_enabled (Optional)
+- client_certificate_mode (Optional)
+- client_certificate_exclusion_paths (Optional)
+- connection_string_blocks (Optional)
+- content_share_force_disabled (Optional)
+- daily_memory_time_quota (Optional)
+- enabled (Optional)
+- ftp_publish_basic_authentication_enabled (Optional)
+- functions_extension_version (Optional)
+- https_only (Optional)
+- public_network_access_enabled (Optional)
+- identity_block (Optional)
+- key_vault_reference_identity_id (Optional)
+- storage_account_blocks (Optional)
+- sticky_settings_block (Optional)
+- storage_account_access_key (Optional)
+- storage_account_name (Optional)
+- storage_uses_managed_identity (Optional)
+- storage_key_vault_secret_id (Optional)
+- tags (Optional)
+- virtual_network_subnet_id (Optional)
+- webdeploy_publish_basic_authentication_enabled (Optional)
+- zip_deploy_file (Optional)
+- timeouts_block (Optional)
+```
+
+### Output:
+
+```bash
+- id
+- name
+- custom_domain_verification_id
+- default_hostname
+- hosting_environment_id
+- identity
+- kind
+- outbound_ip_address_list
+- outbound_ip_addresses
+- possible_outbound_ip_address_list
+- possible_outbound_ip_addresses
+- site_credential
+```
+
+###### More information can be found on the official document [linux_function_app](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_function_app)
---
+++ Azure/modules/LinuxFunctionApp/outputs.tf
@@ -0,0 +1,59 @@
+output "id" {
+  description = "The ID of the Windows Function App."
+  value       = azurerm_linux_function_app.linux_function_app.id
+}
+
+output "name" {
+  description = "The Name of the Windows Function App."
+  value       = azurerm_linux_function_app.linux_function_app.name
+}
+
+output "custom_domain_verification_id" {
+  description = "The identifier used by App Service to perform domain ownership verification via DNS TXT record."
+  value       = azurerm_linux_function_app.linux_function_app.custom_domain_verification_id
+}
+
+output "default_hostname" {
+  description = "The default hostname of the Windows Function App."
+  value       = azurerm_linux_function_app.linux_function_app.default_hostname
+}
+
+output "hosting_environment_id" {
+  description = "The ID of the App Service Environment used by Function App."
+  value       = azurerm_linux_function_app.linux_function_app.hosting_environment_id
+}
+
+output "identity" {
+  description = "An identity block as defined below."
+  value       = azurerm_linux_function_app.linux_function_app.identity
+}
+
+output "kind" {
+  description = "The Kind value for this Windows Function App."
+  value       = azurerm_linux_function_app.linux_function_app.kind
+}
+
+output "outbound_ip_address_list" {
+  description = "A list of outbound IP addresses."
+  value       = azurerm_linux_function_app.linux_function_app.outbound_ip_address_list
+}
+
+output "outbound_ip_addresses" {
+  description = "A comma separated list of outbound IP addresses as a string."
+  value       = azurerm_linux_function_app.linux_function_app.outbound_ip_addresses
+}
+
+output "possible_outbound_ip_address_list" {
+  description = "A list of possible outbound IP addresses, not all of which are necessarily in use."
+  value       = azurerm_linux_function_app.linux_function_app.possible_outbound_ip_address_list
+}
+
+output "possible_outbound_ip_addresses" {
+  description = "A comma separated list of possible outbound IP addresses as a string."
+  value       = azurerm_linux_function_app.linux_function_app.possible_outbound_ip_addresses
+}
+
+output "site_credential" {
+  description = "A site_credential block as defined below."
+  value       = azurerm_linux_function_app.linux_function_app.site_credential
+}
