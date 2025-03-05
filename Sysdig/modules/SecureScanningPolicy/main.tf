8308fb7bcfba0442dda9cf68a4e904abab82b72d
thanos1983 <7160603+thanos1983@users.noreply.github.com>
2025-03-05 15:14:36 +0100

    Testing SecureScanningPolicy module.

--- Sysdig/modules/SecureScanningPolicy/main.tf
+++ Sysdig/modules/SecureScanningPolicy/main.tf
@@ -2,19 +2,19 @@ resource "sysdig_secure_scanning_policy" "secure_scanning_policy" {
   name    = var.name
   comment = var.comment
 
-  dynamic "rules" {
-    for_each = var.rules_blocks
-    content {
-      gate    = rules.value.gate
-      trigger = rules.value.trigger
-      dynamic "params" {
-        for_each = rules.value.params_blocks
-        content {
-          name  = params.value.name
-          value = params.value.name
-        }
-      }
-      action = rules.value.action
-    }
-  }
+  # dynamic "rules" {
+  #   for_each = var.rules_blocks
+  #   content {
+  #     gate    = rules.value.gate
+  #     trigger = rules.value.trigger
+  #     dynamic "params" {
+  #       for_each = rules.value.params_blocks
+  #       content {
+  #         name  = params.value.name
+  #         value = params.value.name
+  #       }
+  #     }
+  #     action = rules.value.action
+  #   }
+  # }
 }
