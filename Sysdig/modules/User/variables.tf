14886c559f046f943935d8fd1e6e67f54724ca30
thanos1983 <7160603+thanos1983@users.noreply.github.com>
2025-04-23 14:32:09 +0200

    Updating Ssysdig User module.

--- Sysdig/modules/User/variables.tf
+++ Sysdig/modules/User/variables.tf
@@ -6,11 +6,7 @@ variable "email" {
 variable "system_role" {
   description = "The privileges for the user. It can be either 'ROLE_USER' or 'ROLE_CUSTOMER'."
   type        = string
-  validation {
-    condition = contains(["ROLE_USER", "ROLE_CUSTOMER"], upper(var.system_role))
-    error_message = "Possible values can be \"ROLE_USER\" or \"ROLE_CUSTOMER\" boolean."
-  }
-  default = "ROLE_USER"
+  default     = null
 }
 
 variable "first_name" {
