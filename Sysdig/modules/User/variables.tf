c0106ba50209eeea212be6aef645d6443edf02d0
thanos1983 <7160603+thanos1983@users.noreply.github.com>
2025-04-24 14:03:18 +0200

    Reverting variable in Sysdig User module.

--- Sysdig/modules/User/variables.tf
+++ Sysdig/modules/User/variables.tf
@@ -6,7 +6,11 @@ variable "email" {
 variable "system_role" {
   description = "The privileges for the user. It can be either 'ROLE_USER' or 'ROLE_CUSTOMER'."
   type        = string
-  default     = null
+  validation {
+    condition = contains(["ROLE_USER", "ROLE_CUSTOMER"], upper(var.system_role))
+    error_message = "Possible values can be \"ROLE_USER\" or \"ROLE_CUSTOMER\" boolean."
+  }
+  default = "ROLE_USER"
 }
 
 variable "first_name" {
