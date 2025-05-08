e4aa68043a75a1f790b4c188e6830e1aac98a8a5
thanos1983 <{ID}+{username}@users.noreply.github.com>
2025-05-08 15:56:01 +0200

    Updating Storage Account Container module output  variables.

--- Azure/modules/StorageContainer/outputs.tf
+++ Azure/modules/StorageContainer/outputs.tf
@@ -12,8 +12,3 @@ output "has_legal_hold" {
   description = "Is there a Legal Hold configured on this Storage Container?"
   value       = azurerm_storage_container.storage_container.has_legal_hold
 }
-
-output "resource_manager_id" {
-  description = "The Resource Manager ID of this Storage Container."
-  value       = azurerm_storage_container.storage_container.resource_manager_id
-}
