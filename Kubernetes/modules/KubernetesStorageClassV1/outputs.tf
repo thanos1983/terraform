a91474ff02bff148ade905dab8d2934c94ba3294
thanos1983 <{ID}+{username}@users.noreply.github.com>
2025-05-09 15:44:59 +0200

    Updating output of kubernetes Storage Class module.

--- Kubernetes/modules/KubernetesStorageClassV1/outputs.tf
+++ Kubernetes/modules/KubernetesStorageClassV1/outputs.tf
@@ -7,3 +7,8 @@ output "metadata" {
   description = "The metadata of this resource."
   value       = kubernetes_storage_class_v1.storage_class_v1.metadata
 }
+
+output "storage_provisioner" {
+  description = "Indicates the type of the provisioner"
+  value       = kubernetes_storage_class_v1.storage_class_v1.storage_provisioner
+}
