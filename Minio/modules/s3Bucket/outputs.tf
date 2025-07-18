f60b58ce11bf65a1d289d81c2519c0a0925c61a9
thanos1983 <{ID}+{username}@users.noreply.github.com>
2025-07-18 17:20:48 +0200

    Updating minio bucket output.

--- Minio/modules/s3Bucket/outputs.tf
+++ Minio/modules/s3Bucket/outputs.tf
@@ -3,6 +3,11 @@ output "id" {
   value       = minio_s3_bucket.s3_bucket.id
 }
 
+output "bucket" {
+  description = "The name of this resource."
+  value       = minio_s3_bucket.s3_bucket.bucket
+}
+
 output "arn" {
   description = "ARN of the bucket."
   value       = minio_s3_bucket.s3_bucket.arn
