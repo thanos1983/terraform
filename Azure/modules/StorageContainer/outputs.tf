a05c9dce8d4c888d7459ede20f0c2b7362b42300
thanos1983 <7160603+thanos1983@users.noreply.github.com>
2025-11-14 14:30:18 +0100

    Updating Storage Account Container module output variables.

--- Azure/modules/StorageContainer/outputs.tf
+++ Azure/modules/StorageContainer/outputs.tf
@@ -3,6 +3,11 @@ output "id" {
   value       = azurerm_storage_container.storage_container.id
 }
 
+output "name" {
+  description = "The name of the Storage Container."
+  value       = azurerm_storage_container.storage_container.name
+}
+
 output "has_immutability_policy" {
   description = "Is there an Immutability Policy configured on this Storage Container?"
   value       = azurerm_storage_container.storage_container.has_immutability_policy
