dc8dd42d783f2d48d86932db9a2e4f2aff7381bf
thanos1983 <7160603+thanos1983@users.noreply.github.com>
2025-11-14 14:54:29 +0100

    Updating Storage Account Container module for variable encryption_scope_override_enabled default value.

--- Azure/modules/StorageContainer/main.tf
+++ Azure/modules/StorageContainer/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_storage_container" "storage_container" {
   storage_account_id                = var.storage_account_id
   container_access_type             = var.container_access_type
   default_encryption_scope          = var.default_encryption_scope
-  encryption_scope_override_enabled = true
+  encryption_scope_override_enabled = false
   metadata                          = var.metadata
 
   dynamic "timeouts" {
