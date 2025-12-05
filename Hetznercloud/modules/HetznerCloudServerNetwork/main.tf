d9d3a366449b36000d01cea1e7e4a6a5a2e40930
thanos1983 <7160603+thanos1983@users.noreply.github.com>
2025-12-05 15:40:05 +0100

    Adding new line on the Hwtzner version files.

--- Hetznercloud/modules/HetznerCloudServerNetwork/main.tf
+++ Hetznercloud/modules/HetznerCloudServerNetwork/main.tf
@@ -1,7 +1,7 @@
 resource "hcloud_server_network" "server_network" {
   server_id  = var.server_id
   alias_ips  = var.alias_ips
+  ip         = var.ip
   network_id = var.network_id
   subnet_id  = var.subnet_id
-  ip         = var.ip
 }
