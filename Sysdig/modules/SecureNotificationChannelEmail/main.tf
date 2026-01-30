9b1e5883dc29291d4ff87ad90724b2bfa1d35fe0
thanos1983 <7160603+thanos1983@users.noreply.github.com>
2026-01-30 12:47:19 +0100

    Updating SecureNotificationChannelEmail Sysdig module.

--- Sysdig/modules/SecureNotificationChannelEmail/main.tf
+++ Sysdig/modules/SecureNotificationChannelEmail/main.tf
@@ -2,7 +2,7 @@ resource "sysdig_secure_notification_channel_email" "secure_notification_channel
   name                    = var.name
   recipients              = var.recipients
   enabled                 = var.enabled
-  notify_when_ok          = var.notify_on_resolve
+  notify_on_resolve       = var.notify_on_resolve
   notify_when_resolved    = var.notify_on_acknowledge
   send_test_notification  = var.send_test_notification
   share_with_current_team = var.share_with_current_team
